a8a9d474d25e9d36a21562485ba6046b9de0784e
dev-kj <rudwns7552@gmail.com>
2020-08-20 11:10:05 +0900

    분해합

---
+++ begginer/src/bruteforce/Exercise_2231.java
@@ -0,0 +1,18 @@
+package bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * @문제 : 분해합
+ * https://www.acmicpc.net/problem/2231
+ */
+public class Exercise_2231 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		
+	}
+}
